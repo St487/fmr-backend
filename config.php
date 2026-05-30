98a1fa0dca8533c26084e719b854d7a233b06679
St487 <siewting8008@gmail.com>
2026-05-31 02:29:25 +0800

    Fix database

--- config.php
+++ config.php
@@ -1,10 +1,12 @@
 <?php
-$servername = "sql309.infinityfree.com";
-$username   = "if0_42053437";
-$password   = "M5AfCcmVbk";
-$dbname     = "if0_42053437_fmrdb";
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $database, $port);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
